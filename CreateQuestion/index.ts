e65389effdd15e1617bc7fa1ae2b922103d53177
IsaRK <isabelle.riverain@gmail.com>
2021-02-01 17:59:33 +0100

    test

--- CreateQuestion/index.ts
+++ CreateQuestion/index.ts
@@ -2,19 +2,21 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import questionService from "../services/questionService";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('CreateQuestion HTTP trigger function processed a request.');
+  context.log('CreateQuestion HTTP trigger function processed a request.');
 
-    let response;
+  let response;
 
-    try {
-        const question = req.body;
-        const result = await questionService.create(question);
-        response = { body: result, status: 200 };
-      } catch (err) {
-        response = { body: err.message, status: 500 };
-      }
+  questionService.init();
 
-      context.res = response;
+  try {
+    const question = req.body;
+    const result = await questionService.create(context, question);
+    response = { body: result, status: 200 };
+  } catch (err) {
+    response = { body: err.message, status: 500 };
+  }
+
+  context.res = response;
 };
 
 export default httpTrigger;
